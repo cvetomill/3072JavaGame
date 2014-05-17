67d9e1c368d2e95f27c54f6a61de3eb95b94d881
emzone <emzone@users.noreply.github.com>
2014-05-17 12:25:18 +0300

    Problems with the logic of moveRight, moveDown and MoveUp fixed.

--- src/game3072/Movement.java
+++ src/game3072/Movement.java
@@ -2,20 +2,20 @@ package game3072;
 
 public class Movement extends game3072.Engine {
 
-	public static void moveLeft(){
+	public static void moveLeft() {
 	     boolean move=false;
-	     for (int y=0;y<mapLimits;y++){
-	    	 for (int x=1;x<mapLimits;x++){
+	     for (int y=0;y<mapLimits;y++) {
+	    	 for (int x=1;x<mapLimits;x++) {
 		    	 if (map[y][x]!=0)
 		    	 {
-		    		 for (int X=x-1;X>=0;X--){
+		    		 for (int X=x-1;X>=0;X--) {
 				    	 if(map[y][X]==0)
 				    	 {
 				    		 move=true;
 				    		 map[y][X]=map[y][x];
 				    		 map[y][x]=0;
 				    	     x=X;
-				    	 } else if (map[y][X]==map[y][x]){
+				    	 } else if (map[y][X]==map[y][x]) {
 				    		 move=true;
 				    		 map[y][X]=(short) (2*map[y][x]);
 				    		 map[y][x]=0;
@@ -33,13 +33,44 @@ public class Movement extends game3072.Engine {
 	     }
 	}
 	
+	public static void moveUp() {
+	     boolean move=false;
+	     for (int x=0;x<mapLimits;x++){
+	    	 for (int y=1; y<mapLimits; y++) { 
+		    	 if (map[y][x]!=0)
+		    	 {
+		    		 for (int Y=y+1; Y<mapLimits; Y++) {
+				    	 if (map[Y][x]==0)
+				    	 {
+				    		 move=true;
+				    		 map[Y][x]=map[y][x];
+				    		 map[y][x]=0;
+				    	     y=Y;
+				    	 } else if (map[Y][x]==map[y][x]) {
+				    		 move=true;
+				    		 map[Y][x]=(short) (2*map[y][x]);
+				    		 map[y][x]=0;
+				    		 break;
+				    	 } else {
+				    		 break;
+				    	 }
+				     }
+		    	 }
+		     } 
+	     }
+	     if (move==true)
+	     {
+	    	 GetRandomPosition();
+	     }
+	}	
+	
 	public static void moveRight(){
 	     boolean move=false;
 	     for (int y=0;y<mapLimits;y++){
-	    	 for (int x=1;x<mapLimits;x++){
+	    	 for (int x=mapLimits - 2; x>=0;x--){
 		    	 if (map[y][x]!=0)
 		    	 {
-		    		 for (int X=x-1;X< mapLimits;X++){
+		    		 for (int X=x+1;X< mapLimits;X++){
 				    	 if (map[y][X]==0)
 				    	 {
 				    		 move=true;
@@ -64,44 +95,13 @@ public class Movement extends game3072.Engine {
 	     }
 	}
 	
-	public static void moveUp() {
-	     boolean move=false;
-	     for (int x=0;x<mapLimits;x++){
-	    	 for (int y=1;y<mapLimits;y++){
-		    	 if (map[y][x]!=0)
-		    	 {
-		    		 for (int Y=y-1;Y<mapLimits;Y++){
-				    	 if (map[Y][x]==0)
-				    	 {
-				    		 move=true;
-				    		 map[Y][x]=map[y][x];
-				    		 map[y][x]=0;
-				    	     y=Y;
-				    	 } else if (map[Y][x]==map[y][x]){
-				    		 move=true;
-				    		 map[Y][x]=(short) (2*map[y][x]);
-				    		 map[y][x]=0;
-				    		 break;
-				    	 } else {
-				    		 break;
-				    	 }
-				     }
-		    	 }
-		     } 
-	     }
-	     if (move==true)
-	     {
-	    	 GetRandomPosition();
-	     }
-	}	
-	
 	public static void moveDown() {
 	     boolean move=false;
 	     for (int x=0;x<mapLimits;x++){
-	    	 for (int y=1;y<mapLimits;y++){
+	    	 for (int y=mapLimits - 2; y>=0; y--){
 		    	 if (map[y][x]!=0)
 		    	 {
-		    		 for (int Y=y-1;Y>=0;Y--){
+		    		 for (int Y=y+1;Y>=0;Y--){
 				    	 if (map[Y][x]==0)
 				    	 {
 				    		 move=true;
@@ -125,4 +125,5 @@ public class Movement extends game3072.Engine {
 	    	 GetRandomPosition();
 	     }
 	}
+	
 }
