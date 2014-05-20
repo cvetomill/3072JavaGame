e5268168d2fd422434d36328b7402078af7c0dd3
emzone <emzone@users.noreply.github.com>
2014-05-20 14:32:42 +0300

    Added additional comments for most of the methods.

--- 3072JavaGame/src/game3072/Engine.java
+++ 3072JavaGame/src/game3072/Engine.java
@@ -7,13 +7,12 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 
 public class Engine {
+	
 	public static int scoreCounter=0;
 	public static byte startValue = 3; // Spawning value
-	public static byte mapLimits = 4; // size of the field
-	public static short[][] map = new short[mapLimits][mapLimits]; // field
-																	// value
-																	// container
-	
+	public static byte mapLimits = 4; // Size of the field
+	public static short[][] map = new short[mapLimits][mapLimits]; // A short two dimensional array responsible for assigning 
+																													  // values to to the corresponding cells
 	
 	public static EventHandler<KeyEvent> keyEventHandler = new EventHandler<KeyEvent>() {
 
@@ -21,7 +20,7 @@ public class Engine {
 			if (keyEvent.getCode() == KeyCode.LEFT) {
 				game3072.Movement.moveLeft(); 
 					// Left arrow code and movement here
-				GameUI.updateStage();
+				GameUI.updateStage(); // Update the graphic image with the new values
 			} else if (keyEvent.getCode() == KeyCode.RIGHT) {
 					game3072.Movement.moveRight();// Right arrow code and movement here
 				GameUI.updateStage();
@@ -33,21 +32,17 @@ public class Engine {
 				// Down arrow code and movement here
 				GameUI.updateStage();
 			}
-			
-			if (GameIsOver())
-			{
-				
-				
+			if (GameIsOver()) // If the game is already over
+			{							
 				GameUI.GameOverStage();
 				System.out.println("Game is over event handler");
-			}
-			
+			}			
 			keyEvent.consume();
 		}
 	};
 	
+	// isThereThisValue - used for determining whether a particular cell is already used
 	private static boolean isThereThisValue(int value) {
-
 		for (int y = 0; y < mapLimits; y++) {
 			for (int x = 0; x < mapLimits; x++) {
 				if (map[y][x] == value) {
@@ -58,57 +53,60 @@ public class Engine {
 		return false;
 	}
 
+	// isUpMoveable - 	a check for possible movement
 	private static boolean isUpMoveable(int y, int x) {
 		if (y > 0) {
-			if (map[y - 1][x] == 0 || map[y - 1][x] == map[y][x]) {
-				return true;
+			if (map[y - 1][x] == 0 || map[y - 1][x] == map[y][x]) { // If the cell which Up from the current position is empty (=0) or 
+				return true; 															 // it has the same value as the current one, proceed
 			}
 		}
 		return false;
 	}
 
+	// isDownMoveable - a check for possible movement
 	private static boolean isDownMoveable(int y, int x) {
 		if (y < mapLimits - 1) {
-			if (map[y + 1][x] == 0 || map[y + 1][x] == map[y][x]) {
-				return true;
+			if (map[y + 1][x] == 0 || map[y + 1][x] == map[y][x]) { // If the cell which Down from the current position is empty (=0) or 
+				return true; 															   // it has the same value as the current one, proceed
 			}
 		}
 		return false;
 	}
 
+	// isRightMoveable - a check for possible movement
 	private static boolean isRightMoveable(int y, int x) {
 		if (x < mapLimits - 1) {
-			if (map[y][x + 1] == 0 || map[y][x + 1] == map[y][x]) {
-				return true;
+			if (map[y][x + 1] == 0 || map[y][x + 1] == map[y][x]) { // If the cell which Right from the current position is empty (=0) or 
+				return true; 															   // it has the same value as the current one, proceed
 			}
 		}
 		return false;
 	}
 
+	// isLeftMoveable - a check for possible movement
 	private static boolean isLeftMoveable(int y, int x) {
 		if (x > 0) {
-			if (map[y][x - 1] == 0 || map[y][x - 1] == map[y][x]) {
-				return true;
+			if (map[y][x - 1] == 0 || map[y][x - 1] == map[y][x]) { 	// If the cell which Left from the current position is empty (=0) or 
+				return true; 															    // it has the same value as the current one, proceed
 			}
 		}
 		return false;
 	}
 
+	// Checking if the game should be still active
 	public static boolean GameIsOver() {
 		for (int y = 0; y < mapLimits; y++) {
 			for (int x = 0; x < mapLimits; x++) {
 				if (map[y][x] != 0) {
-					if (isUpMoveable(y, x) || isDownMoveable(y, x)
-							|| isLeftMoveable(y, x) || isRightMoveable(y, x)) {
-						return false;
+					if (isUpMoveable(y, x) || isDownMoveable(y, x)	// If the movement Up, Down
+							|| isLeftMoveable(y, x) || isRightMoveable(y, x)) { // Left or Right is possible and allowed (true)
+						return false;															// return false - the game continues
 					}
 				} else
 					return false;
 			}
-		}
-		
-		
-		return true;
+		}	
+		return true; // if it returns true, the game is over
 	}
 
 	public static void Run() {
@@ -116,6 +114,7 @@ public class Engine {
 		}
 	}
 
+	// A method which generates values in the still unused cells
 	public static void GetRandomPosition() {
 		Random coord = new Random();
 		int y = coord.nextInt(mapLimits);
